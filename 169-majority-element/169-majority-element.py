e2f8e7a01d643c86e5cce28f6042085698fbb790
Avinash Anantharamu <naashonomics@gmail.com>
2022-07-09 15:52:49 -0700

    Time: 170 ms (95.04%), Space: 15.6 MB (34.61%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,10 +1,19 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        # Boyer-Moore Voting Algorithm
+            count=0
+            candidate=None
+            for n in nums:
+                if count == 0:
+                    candidate=n
+                count += (1 if n == candidate else -1 )
+            return candidate
+    
+    def majorityElement_sort(self, nums: List[int]) -> int:
         #Time complexity : O(nlgn)
-        #Space complexity : O(1) or O(n)
+        #Space complexity : O(1)
         nums.sort()
         return nums[len(nums)//2]
-    
     
     def majorityElement_hashmap(self, nums: List[int]) -> int:
         #Time complexity : O(n)
